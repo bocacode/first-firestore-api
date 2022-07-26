b44c93a1cab389400093cee02a9c34542ee27875
Todd Albert <todd@bocacode.com>
2022-07-26 14:40:33 -0400

    created simple express app

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3002;
+app.use(express.json());
+
+// put our routes here
+app.get('/', (req, res) => {
+  res.send('😀😀😀 Express is working 😀😀😀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}/ ...`);
+});
